8963fbb81e42984370643f8563c2b283c8fd6f44
Shan-e-Shafiq <shafiqshane6@gmail.com>
2024-03-10 22:46:32 +0500

    Changes done

--- db.js
+++ db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
 
-// const mongodb_URI = process.env.MONGODB_URI
-const mongodb_URI = 'mongodb+srv://Shan6:LIU20IfoubeH9oaW@cluster0.zsjkcgs.mongodb.net/iNotebook'
+const mongodb_URI = process.env.MONGODB_URI
 
 function connect_to_database() {
   return new Promise((resolve, reject) => {
