151f7717d0f57bf0f47babeb46f80ff9f43b6655
Shan-e-Shafiq <shafiqshane6@gmail.com>
2024-03-11 22:23:10 +0500

    changes

--- index.js
+++ index.js
@@ -5,29 +5,33 @@ import { notes_route } from "./Routes/notes.js";
 import dotenv from "dotenv"
 import cors from "cors";
 
+async function connect() {
+  console.log(await connect_to_database())
+}
+connect()
+
 const app = express()
 dotenv.config()
+
 
 // MIDDLEWARE
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
   origin: 'https://i-notebook-frontend-one.vercel.app',
-  methods: ['GET', 'POST','PUT','DELETE']
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
 
 // AVAILABLE ROUTES
 app.use('/api/auth', auth_route)
 app.use('/api/notes', notes_route)
 app.get('/', async (req, res) => {
-  // let db = await connect_to_database()
-  res.status(200).json({ 'msg': "SERVER IS UP", "db": db })
+  res.status(200).json({ 'msg': "SERVER IS UP", })
 })
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, async () => {
-  console.log(await connect_to_database())
   console.log('Listening on port:' + port)
 })
 
